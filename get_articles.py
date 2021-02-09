9e50e6cf18cf6123146a57a483671d2e3caa8636
Teodor Stefan Pintea <stevenpintea@gmail.com>
2021-02-09 17:37:15 +0000

    Implemented feature that fileres out the reports that do not have full text available and started to gather statistics.

--- get_articles.py
+++ get_articles.py
@@ -30,33 +30,56 @@ def get_pmc_ids(number = 0, requested_year = datetime.datetime.now().year):
     '''
     results = []
     while(True):
-        handle = Entrez.esearch(db = "pmc", term = "", mindate = str(requested_year), maxdate = str(requested_year), retstart = number, retmax = 100000)
-        record = Entrez.read(handle)
-        handle.close()
+        try:
+            handle = Entrez.esearch(db = "pmc", term = "", mindate = str(requested_year), maxdate = str(requested_year), retstart = number, retmax = 100000)
+            record = Entrez.read(handle)
+            handle.close()
 
-        if(len(record['IdList']) == 0):
-            print("DONE")
-            break
+            if(len(record['IdList']) == 0):
+                print("DONE")
+                break
 
-        number += 100000
-        results.extend(record['IdList'])
+            number += 100000
+            results.extend(record['IdList'])
     
+        except Exception as e:
+            print(str(e) + " when retrieving list of ids")
+
     return results
 
+def hasFullText(article):
+    '''
+        Method that cheks if the publishers of the article allow a user to download the full text of an article for free.
+    '''
+
+    if "The publisher of this article does not allow downloading of the full" in article:
+        return True
+
+    return False
+
 # in development
+
+how = 0
+
 def get_full_text(id):
     '''
         Method that returns the full text of singular bodies such as: Abstract, Introduction, Methods, Results, Discussion
         Any additional fields that might be relevant will be introduced.
-
-
     '''
-
-    handle = Entrez.efetch(db = 'pmc', id = id, retmode = 'xml')
-    record = Medline.parse(handle)
-    for i in record:
-        pp.pprint(i)
-    handle.close()
+    global how
+    try:
+        handle = Entrez.efetch(db = 'pmc', id = id, retmode = 'xml')
+        record = handle.read().decode('utf-8')
+        if hasFullText(record): how = how + 1
+        else:
+            pp.pprint(record) 
+            pass
+        handle.close()   
+    except Exception as e:
+        print(str(e) + " for ID:" + str(id))
+    
+    
+    
     
 # set the email for entrez and configure the pretty printer
 Entrez.email = "Test@example.org"
@@ -69,10 +92,16 @@ results = get_pmc_ids()
 
 finish = time.perf_counter()
 
-print(len(results))
-
 print(f'Finished in {round(finish - start, 2)} seconds(s)')
 
+for index, docID in enumerate([7760437]):
+    if index % 25 == 0 and index > 0:
+        time.sleep(3)
+        print("Avoid an error created by the script requesting information to often.")
+        print("To process: " + str(len(results)))
+        print("Processed: " + str(index))
+        print("Unable: " + str(how))
+    get_full_text(id = str(docID))
 
-
+print("FINAL   " +   str(how))
 
