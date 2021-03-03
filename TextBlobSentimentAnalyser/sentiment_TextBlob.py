94192416855ce3eed0a867c1fd3f074fe12a6648
Teodor Stefan Pintea <stevenpintea@gmail.com>
2021-03-03 18:58:42 +0000

    Created another analyser object that can be reused in the future

---
+++ TextBlobSentimentAnalyser/sentiment_TextBlob.py
@@ -0,0 +1,15 @@
+from textblob import TextBlob
+from collections import OrderedDict
+
+class TextBlobAnalyser:
+    def __init__(self):
+        pass
+
+    def analyse_sentence(self, sentence):
+        # parameter can be a paragraph or a sentence
+        blob = TextBlob(sentence)
+        return float(blob.sentiment.polarity)
+
+    def words_assessed(self, sentence):
+        blob = TextBlob(sentence)
+        return blob.sentiment_assessments
